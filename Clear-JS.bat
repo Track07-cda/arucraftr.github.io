ca0b6c3fe0897ab416666ee25c50ff422a0aa7e3
sophie_desu <52559014+henryEX@users.noreply.github.com>
2021-02-20 19:49:06 +0800

    跳过已有文件

--- Clear-JS.bat
+++ Clear-JS.bat
@@ -1,4 +1,4 @@
 cd js
-java -jar "%~dp0/closure-compiler.jar" -O SIMPLE scrollreveal.js --js_output_file scrollreveal.min.js
-java -jar "%~dp0/closure-compiler.jar" -O SIMPLE general.js --js_output_file general.min.js
+if not exist scrollreveal.min.js java -jar "%~dp0/closure-compiler.jar" -O SIMPLE scrollreveal.js --js_output_file scrollreveal.min.js
+if not exist general.min.js java -jar "%~dp0/closure-compiler.jar" -O SIMPLE general.js --js_output_file general.min.js
 pause
