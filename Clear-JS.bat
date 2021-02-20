00ee37a6d0feb094d9b051419a9016174948f8b5
sophie_desu <52559014+henryEX@users.noreply.github.com>
2021-02-20 11:10:22 +0800

    分离压缩防止玄学问题

--- Clear-JS.bat
+++ Clear-JS.bat
@@ -1,3 +1,3 @@
 cd js
-java -jar "%~dp0/closure-compiler.jar" -O SIMPLE appear.js materialize.js scrollreveal.js general.js --js_output_file models.min.js
+java -jar "%~dp0/closure-compiler.jar" -O SIMPLE scrollreveal.js general.js --js_output_file models.min.js
 pause
