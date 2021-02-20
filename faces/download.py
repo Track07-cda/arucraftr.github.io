3428cab8352093b7c6fb1c687f07724225f3f048
sophie_desu <52559014+henryEX@users.noreply.github.com>
2021-02-20 17:42:53 +0800

    头像

---
+++ faces/download.py
@@ -0,0 +1,11 @@
+import requests
+
+names = ['Krakened76', 'Asaka_aqua', 'Cyan_Bunny', 'tiange98', 'Garry_Lokei', 'circuit_Z', 'Edgardo_qwq', 'saber977', 'Dazzlingme', '9yiyiyi', 'FeiNiaoCou', 'chen1979', 'Aiba_Yukina', 'bigyummyy', 'chen1256', '19hugo', 'Bugalin2007', '114514yeshou', '1600425548', 'SmileyJay', 'FatCat', 'Selimers', 'sophie_desu', 'zhangyi123', 'mieamie', 'Kakng', 'tapia54', 'Track07_cda', 'Chirski_Eidu', 'HGwolf', 'YGOOB', 'Tao_xing']
+
+for name in names:
+    url = 'https://minecraftskinstealer.com/api/v1/skin/download/face/' + name
+    face = requests.get(url)
+    playFile = open(name + '.jpg' ,'wb')
+    playFile.write(face.content)
+
+print('完成')
---
+++ faces/outhtml.py
@@ -0,0 +1,7 @@
+names = ['Krakened76', 'Asaka_aqua', 'Cyan_Bunny', 'tiange98', 'Garry_Lokei', 'circuit_Z', 'Edgardo_qwq', 'saber977', 'Dazzlingme', '9yiyiyi', 'FeiNiaoCou', 'chen1979', 'Aiba_Yukina', 'bigyummyy', 'chen1256', '19hugo', 'Bugalin2007', '114514yeshou', '1600425548', 'SmileyJay', 'FatCat', 'Selimers', 'sophie_desu', 'zhangyi123', 'mieamie', 'Kakng', 'tapia54', 'Track07_cda', 'Chirski_Eidu', 'HGwolf', 'YGOOB', 'Tao_xing']
+names.sort()
+
+def outhtml():
+    for name in names:
+        print('<li>'+'<img src="'+'/faces/'+name+'.jpg'+'"'+'<p>'+name+'</p></li>')
+outhtml()
